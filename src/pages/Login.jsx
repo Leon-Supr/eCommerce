52c7d1340ef97315c71721089419d3aabf1c8806
Leonardo Arguello <Leon-Supr@users.noreply.github.com>
2025-01-20 22:34:41 -0600

    Add base react-hook-form to login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import '@/styles/form.css'
 import logo from '@/assets/icon.png'
+import { useForm } from 'react-hook-form'
 
 const Login = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data);
+
+
   return (
     <>
       <main className="form-signin w-100 m-auto">
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <img
             className="mb-4"
             src={logo}
@@ -14,23 +25,29 @@ const Login = () => {
             width={72}
             height={57}
           />
-          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+          <h1 className="h3 mb-3 fw-normal">Please login</h1>
+          
           <div className="form-floating">
             <input
               type="email"
               className="form-control"
               id="floatingInput"  
               placeholder="name@example.com"
+              {...register('loginEmail', { required: true })} //El register debe ir dentro del input, para que sepa cuál se registrará 
             />
+            {errors.loginEmail && <span>This field is required</span>}
             <label htmlFor="floatingInput">Email address</label>
           </div>
+          
           <div className="form-floating">
             <input
               type="password"
               className="form-control"
               id="floatingPassword"
               placeholder="Password"
+              {...register('loginPassword', { required: true })}
             />
+            {errors.loginPassword && <span>This field is required</span>}
             <label htmlFor="floatingPassword">Password</label>
           </div>
           <button className="btn btn-primary w-100 py-2" type="submit">
